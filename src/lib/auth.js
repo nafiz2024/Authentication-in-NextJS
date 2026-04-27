46406be5655d032a30e9657377ba5c8dd159405b
nafiz Alam <nafizalam.dev@gmail.com>
2026-04-28 01:15:44 +0600

    Create MongoDB account and MongoDB client

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient("mongodb://localhost:27017/database");
+const client = new MongoClient(process.env.AUTH_DB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
